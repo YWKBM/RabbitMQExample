e05e4761ce3920d2678aba656c0b80cc83b6dffa
ywkbm <ywkbm1973@gmail.com>
2024-05-06 00:27:23 +0500

    fix

--- coingeckoStat/CoingeckoDb/DBConfig.cs
+++ coingeckoStat/CoingeckoDb/DBConfig.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CoingeckoDb
+namespace CoingeckoDb;
+
+public class DBConfig
 {
-    public class DBConfig
-    {
-        
-    }
+    public string ConnectionString { get; set; } = string.Empty;
 }
--- coingeckoStat/CoingeckoDb/DbRegExtensions.cs
+++ coingeckoStat/CoingeckoDb/DbRegExtensions.cs
@@ -4,8 +4,10 @@ namespace CoingeckoDb;
 
 public static class DbRegExtensions
 {
-    public static IServiceCollection AddDb(this IServiceCollection services)
+    public static IServiceCollection AddDb(this IServiceCollection services, string ConnectionString)
     {
+        services.AddSingleton<DBConfig>();
+        
         EntityFrameworkServiceCollectionExtensions.AddDbContext<AppDbContext>(services);
         return services;
     }
